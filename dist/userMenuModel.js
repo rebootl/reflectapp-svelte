44cabde6767569311b3056ddb0e2f39f97149fc1
Cem Aydin <cem.aydin@gmx.ch>
2020-07-12 21:15:33 +0200

    usermenu endpoint (public)

--- dist/userMenuModel.js
+++ dist/userMenuModel.js
@@ -7,11 +7,7 @@ async function getPublicTopics(db, user) {
                 ] } },
         { $unwind: "$topics" },
         { $group: { _id: "$topics" } },
-        /*{ $unwind: "$tags" },*/
-        /*{ $group: { _id: "$tags" }},*/
-        /*{ $project: { _id: -1, name: "$_id", topics: "$topics" }},*/
-        /*{ $unwind: "$topics" },*/
-        { $project: { _id: -1, name: "$_id" } },
+        { $project: { _id: 0, name: "$_id" } },
         { $sort: { name: 1 } }
     ];
     return await c.aggregate(q).toArray();
@@ -24,21 +20,9 @@ async function getPublicTags(db, user, topic) {
                     { private: false },
                     { topics: topic }
                 ] } },
-        // { text: "blabla", [ 'star trek', 'sci-fi' ] }
-        // { text: "foo", [ 'star trek', 'movie' ] }
-        // { text: "boo", [ 'babylon 5' ] }
         { $unwind: "$tags" },
-        // { text: "blabla", 'star trek' }
-        // { text: "blabla", 'sci-fi' }
-        // { text: "foo", 'star trek' }
-        // { text: "foo", 'movie' }
-        // { text: "boo", 'babylon 5' }
         { $group: { _id: "$tags" } },
-        // { 'star trek' }
-        // { 'sci-fi' }
-        // { 'movie' }
-        // { 'babylon 5' }
-        { $project: { _id: -1, name: "$_id" } },
+        { $project: { _id: 0, name: "$_id" } },
         { $sort: { name: 1 } }
     ];
     return await c.aggregate(q).toArray();
@@ -49,7 +33,7 @@ async function getPublicUserMenu(db, user) {
     for (const topic of topics) {
         const tags = await getPublicTags(db, user, topic.name);
         res.push({
-            topic: topic.name,
+            name: topic.name,
             tags: tags.map((t) => t.name)
         });
     }
